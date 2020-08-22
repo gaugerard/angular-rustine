b989ddee5b080735ec80187a1f837bccc7967661
Nathan-Mathieu <nathan.mathieu@gmail.com>
2020-08-22 16:03:45 +0200

    cross in craft list

--- src/assets/js/dropWipe.js
+++ src/assets/js/dropWipe.js
@@ -5,7 +5,6 @@ function dropWipe() {
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.btn-drop-wipe')) {
-    console.log('rreerfref');
     var dropdowns = document.getElementsByClassName("drop-wipe-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
